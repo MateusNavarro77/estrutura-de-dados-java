f52d009d2863d72b195db67e1139396784530cd6
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-11-11 11:57:36 -0300

    feat: add WeightedEdge class for Dijkstra's algorithm

---
+++ src/main/java/com/mateusnavarro77/algoritmos/dijkstra/WeightedEdge.java
@@ -0,0 +1,26 @@
+package com.mateusnavarro77.algoritmos.dijkstra;
+
+public class WeightedEdge {
+    private int v;
+    private int w;
+    private double weight;
+
+    public WeightedEdge(int v, int w, double weight) {
+
+        this.v = v;
+        this.w = w;
+        this.weight = weight;
+    }
+
+    public double weight() {
+        return weight;
+    }
+
+    public int from() {
+        return v;
+    }
+
+    public int to() {
+        return w;
+    }
+}
